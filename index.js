6d3834d11512f7aac8259f73981151cd34d0de39
aumu1530 <aumu1530@gmail.com>
2024-07-05 19:47:03 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,66 @@
+let h1 = document.getElementById("h1")
+
+h1.onclick = function() {
+  h1.style.backgroundColor = "red"
+  h1.style.color = "#fff"
+
+}
+
+let p = document.getElementById("p")
+
+p.onclick = function () {
+    p.style.backgroundColor = 'yellow'
+    p.style.color = 'red'
+}
+
+let h2 = document.getElementById("h2")
+
+h2.onclick = function() {
+  h2.style.backgroundColor = "red"
+  h2.style.color = "#fff"
+
+}
+
+let p2 = document.getElementById("p2")
+
+p2.onclick = function () {
+    p2.style.backgroundColor = 'yellow'
+    p2.style.color = 'red'
+}
+
+let div = document.getElementById("div")
+
+div.onclick = function () {
+    div.style.backgroundColor = 'black'
+    
+}
+
+
+let div2 = document.getElementById("div2")
+
+div2.onclick = function () {
+    div2.style.backgroundColor = 'lime'
+    
+}
+
+let span = document.getElementById("span")
+
+span.onclick = function () {
+    span.style.backgroundColor = 'black'
+    
+}
+
+
+let icons = document.getElementById("icons")
+
+icons.onclick = function () {
+    icons.style.backgroundColor = 'lime'
+    
+}
+
+let nav = document.getElementById("nav")
+
+nav.onclick = function () {
+    nav.style.backgroundColor = 'lime'
+    
+}
